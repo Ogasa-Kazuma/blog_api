ebd9a274e3e91c814a92f13c39cc7d93f5cb03ae
Ogasa-Kazuma <knct0831@gmail.com>
2023-02-24 07:32:49 +0000

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+func main() {
+
+}
