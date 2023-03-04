c979ffc48e9a4fce2ce5eb5c780c7c4470990f34
Ogasa-Kazuma <knct0831@gmail.com>
2023-03-04 10:12:11 +0000

    検索

--- cmd/main.go
+++ cmd/main.go
@@ -11,18 +11,20 @@ import (
 	//	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
-func searchFreeWord() {
-	dbClientSession := session.Must(session.NewSession())
-	// Create a DynamoDB client with additional configuration
-	ddb := dynamodb.New(dbClientSession, aws.NewConfig().WithRegion("ap-northeast-1"))
+const articleTableName string = "article"
+const articleContentTableName string = "contents"
+const region = "ap-northeast-1"
 
-	searchWord := "content"
+func searchArticlesByString(targetString string) {
+	dbClientSession := session.Must(session.NewSession())
+	ddb := dynamodb.New(dbClientSession, aws.NewConfig().WithRegion(region))
+
 	params := &dynamodb.ScanInput{
-		TableName:        aws.String("articles"),
+		TableName:        aws.String(articleContentTableName),
 		FilterExpression: aws.String("contains(content, :search_text)"),
 		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
 			":search_text": {
-				S: aws.String(searchWord),
+				S: aws.String(targetString),
 			},
 		},
 	}
@@ -34,22 +36,77 @@ func searchFreeWord() {
 	fmt.Println(res)
 }
 
-func queryByPkAndSk(article_id string, sk string) {
+func searchArticles(article_id string, sk_attr string) {
 	dbClientSession := session.Must(session.NewSession())
-	// Create a DynamoDB client with additional configuration
-	ddb := dynamodb.New(dbClientSession, aws.NewConfig().WithRegion("ap-northeast-1"))
+	ddb := dynamodb.New(dbClientSession, aws.NewConfig().WithRegion(region))
 
-	params := &dynamodb.QueryInput{
-		TableName:              aws.String("articles"),
-		KeyConditionExpression: aws.String("article_id = :id AND SK = :SK"),
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":id": {
-				S: aws.String(article_id),
+	// SKが指定されている場合
+	var params *dynamodb.QueryInput
+	if sk_attr != "" {
+		params = &dynamodb.QueryInput{
+			TableName:              aws.String(articleTableName),
+			KeyConditionExpression: aws.String("article_id = :id AND SK = :SK"),
+			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+				":id": {
+					S: aws.String(article_id),
+				},
+				":SK": {
+					S: aws.String(sk_attr),
+				},
 			},
-			":SK": {
-				S: aws.String(sk),
+		}
+	} else {
+		params = &dynamodb.QueryInput{
+			TableName:              aws.String(articleTableName),
+			KeyConditionExpression: aws.String("article_id = :id"),
+			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+				":id": {
+					S: aws.String(article_id),
+				},
 			},
-		},
+		}
+	}
+
+	res, err := ddb.Query(params)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(res)
+}
+
+func sortByPostedDate(createdAt articleCreatedAtInput) {
+	dbClientSession := session.Must(session.NewSession())
+	ddb := dynamodb.New(dbClientSession, aws.NewConfig().WithRegion(region))
+
+	var params *dynamodb.QueryInput
+	if createdAt.date != "" {
+		params = &dynamodb.QueryInput{
+			TableName: aws.String(articleTableName),
+			IndexName: aws.String("created_ym-created_dms-index"),
+			// ソートキーをcategories#TRIPなど、1つ1つのcategoryごとにキーを分けているため、このクエリを実行しやすい
+			KeyConditionExpression: aws.String("created_ym = :year_month AND begins_with(created_dms, :created_date)"),
+			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+				":year_month": {
+					S: aws.String(createdAt.year + "-" + createdAt.month),
+				},
+				":created_date": {
+					S: aws.String(createdAt.date),
+				},
+			},
+		}
+	} else {
+		params = &dynamodb.QueryInput{
+			TableName: aws.String(articleTableName),
+			IndexName: aws.String("created_ym-created_dms-index"),
+			// ソートキーをcategories#TRIPなど、1つ1つのcategoryごとにキーを分けているため、このクエリを実行しやすい
+			KeyConditionExpression: aws.String("created_ym = :year_month"),
+			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+				":year_month": {
+					S: aws.String(createdAt.year + "-" + createdAt.month),
+				},
+			},
+		}
 	}
 	res, err := ddb.Query(params)
 	if err != nil {
@@ -59,36 +116,23 @@ func queryByPkAndSk(article_id string, sk string) {
 	fmt.Println(res)
 }
 
-func reqArticlesByCategories(article_id string, sk string) {
-	dbClientSession := session.Must(session.NewSession())
-	// Create a DynamoDB client with additional configuration
-	ddb := dynamodb.New(dbClientSession, aws.NewConfig().WithRegion("ap-northeast-1"))
-
-	params := &dynamodb.QueryInput{
-		TableName:              aws.String("articles"),
-		KeyConditionExpression: aws.String("article_id = :id AND begins_with(SK, :SK)"),
-		// FilterExpression:       aws.String("begins_with(categories, )"),
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":id": {
-				S: aws.String(article_id),
-			},
-			":SK": {
-				S: aws.String(sk),
-			},
-		},
-	}
-	res, err := ddb.Query(params)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(res)
+type articleCreatedAtInput struct {
+	year  string
+	month string
+	date  string
 }
 
 func main() {
-	// searchFreeWord()
+	searchArticles("id1", "title")
+	// 月単位での読み取りと、日付指定の読み取りが可能
+	// 昇順並び替えのための、何時とか何分とかもレスポンスされる
+	createdAtToSearch := articleCreatedAtInput{"2023", "2", "25"}
+	sortByPostedDate(createdAtToSearch)
+
+	// searchByTitle()
+	// searchArticlesByString("test_string")
 	// queryByPkAndSk("id1", "title")
-	reqArticlesByCategories("id1", "t")
+	// reqArticlesByCategories("id1", "TRIP")
 	// フリーワード検索
 	// カテゴリ別検索
 	// 新着順検索
